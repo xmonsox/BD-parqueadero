a0d40d655d23926d7a8031225e1d91523790103b
Maribel Obando Joven <maribel-obando.j@utp.edu.co>
2023-06-22 12:05:44 -0500

    bd_parqueadero

--- parqueadero.sql
+++ parqueadero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-06-2023 a las 03:36:06
+-- Tiempo de generación: 22-06-2023 a las 19:04:26
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,14 +24,74 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `administradores`
+-- Estructura de tabla para la tabla `ingresos`
 --
 
-CREATE TABLE `administradores` (
-  `nombre` varchar(255) NOT NULL,
-  `cedula` varchar(15) NOT NULL,
-  `telefono` double NOT NULL
+CREATE TABLE `ingresos` (
+  `id` int(11) NOT NULL,
+  `placa` varchar(255) NOT NULL,
+  `tipo_vehiculo` varchar(255) NOT NULL,
+  `tipo_pago` varchar(255) NOT NULL,
+  `fecha_ingreso` varchar(255) NOT NULL,
+  `nombre_espacio` varchar(10) NOT NULL,
+  `estado` enum('activo','inactivo','','') NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `ingresos`
+--
+
+INSERT INTO `ingresos` (`id`, `placa`, `tipo_vehiculo`, `tipo_pago`, `fecha_ingreso`, `nombre_espacio`, `estado`) VALUES
+(1, 'KNH28F', 'Moto', 'Dia', 'Mon Jun 19 23:24:51 COT 2023', 'm1', 'activo'),
+(2, 'ANM548', 'Carro', 'Hora', 'Mon Jun 19 23:26:58 COT 2023', 'c9', 'activo'),
+(3, 'ALB47F', 'Moto', 'Dia', 'Thu Jun 22 10:42:17 COT 2023', 'M2', 'activo'),
+(4, 'MHL49G', 'Carro', 'Dia', 'Thu Jun 22 10:43:06 COT 2023', 'C1', 'activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `membresias`
+--
+
+CREATE TABLE `membresias` (
+  `propietario` varchar(255) NOT NULL,
+  `telefono` double NOT NULL,
+  `tipo_vehiculo` varchar(255) NOT NULL,
+  `tipo_membresia` varchar(255) NOT NULL,
+  `placa` varchar(255) NOT NULL,
+  `fecha_inicio` varchar(255) NOT NULL,
+  `fecha_vencimiento` varchar(255) NOT NULL,
+  `total` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `membresias`
+--
+
+INSERT INTO `membresias` (`propietario`, `telefono`, `tipo_vehiculo`, `tipo_membresia`, `placa`, `fecha_inicio`, `fecha_vencimiento`, `total`) VALUES
+('Andrey', 3103836118, 'Moto', 'Mensual', 'KHJ43F', 'Tue Jun 20 10:32:46 COT 2023', 'Thu Jul 20 10:32:46 COT 2023', 0),
+('Monsalve', 314785621, 'Carro', 'Mensual', 'BLQ47F', 'Tue Jun 20 18:00:40 COT 2023', 'Wed Jul 05 18:00:40 COT 2023', 40000),
+('MonsoDev', 3457896255, 'Carro', 'Mensual', 'ASQ89F', 'Tue Jun 20 18:08:08 COT 2023', 'Thu Jul 20 18:08:08 COT 2023', 40000);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
